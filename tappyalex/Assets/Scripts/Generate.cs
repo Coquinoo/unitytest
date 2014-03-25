c29b048060e05077d21962519b1b745f93e413a1
coquinoob <alexshow28@hotmail.com>
2014-03-25 13:27:27 +0100

    ceci est mon 1er super commit

---
+++ tappyalex/Assets/Scripts/Generate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+	public GameObject rocks;
+	int score = 0;
+	
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 0.8f, 1.6f);
+	}
+	
+	// Update is called once per frame
+	void OnGUI () 
+	{
+		GUI.color = Color.black;
+		GUILayout.Label(" Score: " + score.ToString());
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(rocks);
+		score++;
+	}
+}
